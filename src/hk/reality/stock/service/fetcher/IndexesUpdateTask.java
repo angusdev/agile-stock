a76c0c79dd542771a1763f7fb3949adbf8bf1d42
siuying <siu.ying@gmail.com>
2009-11-04 01:05:21 +0800

    add error handling for indexes retrival

--- src/hk/reality/stock/service/fetcher/IndexesUpdateTask.java
+++ src/hk/reality/stock/service/fetcher/IndexesUpdateTask.java
@@ -1,7 +1,11 @@
 package hk.reality.stock.service.fetcher;
 
 import hk.reality.stock.IndexActivity;
+import hk.reality.stock.PortfolioActivity;
+import hk.reality.stock.R;
 import hk.reality.stock.model.Index;
+import hk.reality.stock.service.exception.DownloadException;
+import hk.reality.stock.service.exception.ParseException;
 import hk.reality.stock.view.IndexAdapter;
 import hk.reality.utils.NetworkDetector;
 
@@ -9,6 +13,7 @@ import java.util.List;
 
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 public class IndexesUpdateTask extends AsyncTask<Void, Integer, Boolean> {
     public static final String TAG = "IndexesUpdateTask";
@@ -34,9 +39,23 @@ public class IndexesUpdateTask extends AsyncTask<Void, Integer, Boolean> {
 
         Log.i(TAG, "start fetcher");
         IndexesFetcher fetcher = IndexesFetcherFactory.getIndexesFetcher(activity);
-        results = fetcher.fetch();
-
-        return Boolean.TRUE;
+        
+        try {
+            results = fetcher.fetch();
+            return Boolean.TRUE;
+        } catch (DownloadException de) {
+            Log.e(TAG, "error downloading stock", de);
+            error = Error.ERROR_DOWNLOAD;
+            return Boolean.FALSE;
+        } catch (ParseException pe) {
+            Log.e(TAG, "error parsing code", pe);
+            error = Error.ERROR_PARSE;
+            return Boolean.FALSE;
+        } catch (RuntimeException re) {
+            Log.e(TAG, "unexpected error while update stock quote", re);
+            error = Error.ERROR_UNKNOWN;
+            return Boolean.FALSE;
+        } 
     }
 
     private void updateIndexes(List<Index> indexes) {
@@ -68,7 +87,22 @@ public class IndexesUpdateTask extends AsyncTask<Void, Integer, Boolean> {
             Log.i(TAG, "update success, number of results ..." + results.size());
             updateIndexes(results);
         } else {
-            Log.i(TAG, "update failure");
+            switch (error) {
+            case ERROR_NO_NET:
+                Toast.makeText(activity, R.string.msg_no_network, Toast.LENGTH_LONG).show();
+                break;
+            case ERROR_DOWNLOAD:
+                activity.showDialog(PortfolioActivity.DIALOG_ERR_QUOTE);
+                break;
+            case ERROR_PARSE:
+                activity.showDialog(PortfolioActivity.DIALOG_ERR_QUOTE_UPDATE);
+                break;
+            case ERROR_UNKNOWN:
+                activity.showDialog(PortfolioActivity.DIALOG_ERR_UNEXPECTED);
+                break;
+            default:
+                break;
+            }
         }
     }
 }
